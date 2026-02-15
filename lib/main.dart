c134dcc01afb939b23fb6923c7230f296bb66357
Adly Wagdy <adly@example.com>
2026-02-15 22:19:41 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const EscrewSystemApp());
+}
+
+class EscrewSystemApp extends StatelessWidget {
+  const EscrewSystemApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
